a8303e079e5c40255bebbf6e2ff07138545a115e
aalto <aaltopiiri@outlook.com>
2019-02-03 18:49:59 +0200

    Edit file goodbye.py

--- goodbye.py
+++ goodbye.py
@@ -1,8 +1,11 @@
-string_1="Goodbye my friend      "
-string_2="     It's hard to die"
+string_1="Goodbye my friend "
+string_2=" It's hard to die"
+msg=string_1+string_2
+msg=string_1.rstrip()+string_2.lower()
+print(msg)
 string_1=string_1.rstrip()
 string_2=string_2.lstrip()
-msg=string_1+'\n'+string_2
+msg='\n\t'+string_1+'\n\t'+string_2+'\n'
 msg=msg.title()
 print(msg)
 
