27d3c297e471ed49ef4f90218219a404bd6b11f0
aalto <aaltopiiri@outlook.com>
2019-02-03 18:41:57 +0200

    Add string variables to file goodbye.py

--- goodbye.py
+++ goodbye.py
@@ -1,4 +1,8 @@
-msg="Goodbye my friend "
+string_1="Goodbye my friend      "
+string_2="     It's hard to die"
+string_1=string_1.rstrip()
+string_2=string_2.lstrip()
+msg=string_1+'\n'+string_2
+msg=msg.title()
 print(msg)
-msg="It's hard to die"
-print(msg)
+
